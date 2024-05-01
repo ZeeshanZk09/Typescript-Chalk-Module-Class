cc7faba20004d29df44930d366cad2b7df2cafb2
Entreprenuer Zeeshan <dr5269139@gmail.com>
2024-05-01 20:34:04 +0500

    Typescript-Chalk-Modules-Class

---
+++ main.ts
@@ -0,0 +1,10 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+let answers =  await  inquirer.prompt([{
+    name : "age",
+    type : "number",    
+    message : "Enter you age:"
+}]);
+
+console.log(chalk.green(`Insha Allah in ${60-answers.age} years you will be  60 years old. `));
